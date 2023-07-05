6b77e36822639e1f2ce015a65824bed5390e670d
digovc <rodrigo.conspena@gmail.com>
2023-07-05 00:31:48 +0000

    Deploying to gh-pages from @ digovc/simple-stage@a23b7af34a91b6ccdd268e4c7455a91e5f388c8e 🚀

---
+++ assets/DangerButton.vue_vue_type_script_setup_true_lang-23fb6551.js
@@ -0,0 +1,1 @@
+import{D as t}from"./music.repository-3056144a.js";import{d as a,k as o,e as s,f as r,a as n,h as _}from"./index-bfed1039.js";const c={class:"hover:bg-red-100 p-2 px-4 text-red-600"},f=a({__name:"DangerButton",setup(d){return(e,l)=>(r(),o(t,null,{default:s(()=>[n("div",c,[_(e.$slots,"default")])]),_:3}))}});export{f as _};
---
+++ assets/playlist.repository-96d101bd.js
@@ -0,0 +1,1 @@
+import{R as s}from"./music.repository-3056144a.js";class e extends s{getTableName(){return"playlist"}}const a=new e;export{a as p};
