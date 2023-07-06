f14745c2943dd988bc03709debe5ff93e6edf16f
digovc <rodrigo.conspena@gmail.com>
2023-07-06 21:25:17 +0000

    Deploying to gh-pages from @ digovc/simple-stage@60271205ea512bb192c667cac64b965a1c809ac0 🚀

--- assets/Splash-a647faa3.js
+++
@@ -1,1 +0,0 @@
-import{d as t,u as o,o as s,c,f as n}from"./index-c397f94a.js";const a={class:"h-full flex place-content-center place-items-center"},m=t({__name:"Splash",setup(r){const e=o();return s(()=>{setTimeout(()=>e.replace("/home"),2e3)}),(l,p)=>(n(),c("div",a," Simple Stage "))}});export{m as default};
